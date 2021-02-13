a3d4f499fcc42ae3882945728ff2da1b1609504b
Rafael Lino <rafael.veloso.lino@hotmail.com>
2021-02-13 08:11:52 -0300

    renaming folders

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-from fastapi import FastAPI
-
-# app = FastAPI()
-
-#
-# @app.get("/")
-# def root():
-#     return {"message": "Hello World"}
-#
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: str):
-#     return {"item_id": item_id}
 import uvicorn
 from uvicorn.config import LOGGING_CONFIG
 import configparser
@@ -18,11 +6,11 @@ config = configparser.ConfigParser(interpolation=None)
 
 if __name__ == '__main__':
     new_config = copy.deepcopy(LOGGING_CONFIG)
-    config.read(".config.init")
+    config.read(".config")
     new_config["formatters"]["access"]["fmt"] = config["logging_config"]["logging_format"]
     debug = config["server_config"].getboolean("debug")
     uvicorn.run(
-        "sql_app.main:app",
+        "app.main:app",
         host="0.0.0.0",
         port=8000,
         reload=True,

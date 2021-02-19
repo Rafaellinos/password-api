dd6a8cc87aaf4766fbc303ae6263b7f7de1e969f
Rafael Lino <rafael.veloso.lino@hotmail.com>
2021-02-18 22:01:41 -0300

    [UPD] config get settings from .config

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,6 +1,7 @@
-from functools import lru_cache
-from pydantic import BaseSettings, PostgresDsn, Field
 import os
+from functools import lru_cache
+from secrets import token_hex
+from pydantic import BaseSettings, PostgresDsn, Field
 
 ENV_FILE = '.config'
 
@@ -8,13 +9,13 @@ ENV_FILE = '.config'
 class Settings(BaseSettings):
     DEBUG: bool = Field(..., env="debug")
     DATABASE_URL: PostgresDsn = Field(..., env="bd_url")
-    SECRET_KEY: str = "afe99645a1078d18524d735a0915f354ed09db42443003938b32b200c802dc4a"
-    APP_NAME = 'PasswordAPP'
-    REGISTRATION_TOKEN_LIFETIME = 60 * 60 * 24 * 7  # one week
-    TOKEN_ALGORITHM = 'HS256'
-    API_PREFIX = '/api'
-    HOST = '0.0.0.0'
-    PORT = 8000
+    SECRET_KEY: str = Field(..., env="secret")
+    APP_NAME: str = Field(..., env="name")
+    REGISTRATION_TOKEN_LIFETIME: int = 60 * 60 * 24 * 7  # one week
+    TOKEN_ALGORITHM: str = 'HS256'
+    API_PREFIX: str = '/api'
+    HOST: str = Field(..., env="host")
+    PORT: int = Field(..., env="port")
     BASE_URL = '{}:{}'.format(HOST, str(PORT))
     ALLOWED_HOSTS: str = ""
 
@@ -25,7 +26,10 @@ class Settings(BaseSettings):
 @lru_cache()
 def get_settings():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    return Settings(
+    settings = Settings(
         _env_file=f'{dir_path}/{ENV_FILE}',
         _env_file_encoding='utf-8',
     )
+    if not settings.SECRET_KEY:
+        settings.SECRET_KEY = token_hex(32)
+    return settings
